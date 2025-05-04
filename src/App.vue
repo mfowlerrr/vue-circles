8154e37521921a71f50edaa03376aa4ec5e9f7dc
Fowler <matty01fowler@gmail.com>
2025-05-04 13:09:02 +0930

    Add multiple red-circles as option, with counter to add/remove more red circles

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,37 @@
 import { ref, computed } from "vue";
 
 const RADIUS = 80;
-
-const circle1 = ref({ x: 50, y: 50 });
-const circle2 = ref({ x: 200, y: 50 });
 const dragging = ref(null);
 
+const circle1 = ref({ x: 50, y: 150 });
+
+const red_count = ref(2);
+const red_circles = ref([
+  { x: 200, y: 150 },
+  { x: 200, y: 250 }
+]);
+
+function removeRedCircle() {
+  red_count.value--;
+  red_circles.value.pop();
+}
+
+function addRedCircle() {
+  red_count.value++;
+  let new_circle = JSON.parse(JSON.stringify(red_circles.value.at(-1)));
+  new_circle.y += 100;
+  red_circles.value.push(new_circle);
+}
+
+const disableAddButton = computed(() => {
+  return !(red_count.value < 5);
+});
+const disableDeleteButton = computed(() => {
+  return !(red_count.value > 1);
+});
+
 function startDrag(event, id) {
-  const circle = id === 1 ? circle1.value : circle2.value;
+  const circle = id === -1 ? circle1.value : red_circles.value[id];
   const offsetX = event.clientX - circle.x;
   const offsetY = event.clientY - circle.y;
 
@@ -22,10 +46,10 @@ function onMouseMove(event) {
   const newX = event.clientX - offsetX;
   const newY = event.clientY - offsetY;
 
-  if (id === 1) {
+  if (id === -1) {
     circle1.value = { x: newX, y: newY };
   } else {
-    circle2.value = { x: newX, y: newY };
+    red_circles.value[id] = { x: newX, y: newY };
   }
 }
 
@@ -36,49 +60,72 @@ function stopDrag() {
 const isTouching = computed(() => {
   const r = RADIUS / 2;
 
+  //green circle
   const center_x_1 = circle1.value.x + r;
   const center_y_1 = circle1.value.y + r;
-  const center_x_2 = circle2.value.x + r;
-  const center_y_2 = circle2.value.y + r;
 
-  const dx = center_x_1 - center_x_2;
-  const dy = center_y_1 - center_y_2;
+  //red circles
+  for (let c of red_circles.value) {
+    const center_x_2 = c.x + r;
+    const center_y_2 = c.y + r;
 
-  const distance = Math.sqrt(dx * dx + dy * dy);
-  return distance < RADIUS;
+    const dx = center_x_1 - center_x_2;
+    const dy = center_y_1 - center_y_2;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < RADIUS) {
+      return true;
+    }
+  }
+
+  return false;
 });
 
-const circle1ZIndex = computed(() =>
-  dragging.value && dragging.value.id === 1 ? 10 : 1
-);
-const circle2ZIndex = computed(() =>
-  dragging.value && dragging.value.id === 2 ? 10 : 1
-);
+//Implementation of higher Z index for the grabbed circle
+// const circle1ZIndex = computed(() =>
+//   dragging.value && dragging.value.id === -1 ? 10 : 1
+// );
+// const circle2ZIndex = computed(() =>
+//   dragging.value && dragging.value.id === 2 ? 10 : 1
+// );
 </script>
 
 <template>
   <div id="app" @mousemove="onMouseMove" @mouseup="stopDrag">
     <div class="indicator">
-      Circles are {{ isTouching ? "touching" : "not touching" }}
+      <div>
+        Green is {{ isTouching ? "touching" : "not touching" }} a red circle
+      </div>
+      <button
+        :disabled="disableAddButton"
+        class="addButton"
+        @click="addRedCircle()"
+      >
+        Add Circle
+      </button>
+      <button :disabled="disableDeleteButton" @click="removeRedCircle()">
+        Remove Circle
+      </button>
     </div>
     <div
       class="circle"
       :style="{
         left: circle1.x + 'px',
         top: circle1.y + 'px',
-        zIndex: circle1ZIndex
+        zIndex: 1
       }"
-      @mousedown="startDrag($event, 1)"
+      @mousedown="startDrag($event, -1)"
     ></div>
 
+    <!-- Red circles loop to show all circles -->
     <div
       class="circle circle2"
+      v-for="(circ, index) in red_circles"
       :style="{
-        left: circle2.x + 'px',
-        top: circle2.y + 'px',
-        zIndex: circle2ZIndex
+        left: circ.x + 'px',
+        top: circ.y + 'px',
+        zIndex: 1
       }"
-      @mousedown="startDrag($event, 2)"
+      @mousedown="startDrag($event, index)"
     ></div>
   </div>
 </template>
@@ -110,12 +157,16 @@ const circle2ZIndex = computed(() =>
   position: absolute;
   width: 80px;
   height: 80px;
-  background-color: red;
+  background-color: green;
   border-radius: 50%;
   cursor: grab;
 }
 
 .circle2 {
-  background-color: green;
+  background-color: red;
+}
+
+.addButton {
+  margin-right: 20px;
 }
 </style>
